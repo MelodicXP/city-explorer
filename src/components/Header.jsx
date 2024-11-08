26b4efa8c6f748284ed67fecdf1a494a68f3a40a
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-11-08 10:28:35 -0600

    Refactor to functional componenet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,9 @@
-import React from 'react';
-
-class Header extends React.Component {
-
-  constructor(props) {
-
-    super(props); // Activates React.Component
-    
-  }
-
-  render() {
-
-    return (
-      <header>
-        <h1>City Explorer</h1>
-      </header>
-    )
-  }
-
-}
+const Header = () => {
+  return (
+    <header>
+      <h1>City Explorer</h1>
+    </header>
+  );
+};
 
 export default Header;
