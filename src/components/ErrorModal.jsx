fd875c18c5af40b8ba5344b266bdbec0ef74be91
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-11-10 06:20:15 -0600

    Add react modal

--- src/components/ErrorModal.jsx
+++ src/components/ErrorModal.jsx
@@ -1,8 +1,27 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 const ErrorModal = () => {
   return (
-    <>
-      <p>This is error Modal</p>
-    </>
+    <div
+    className="modal show"
+    style={{ display: 'block', position: 'initial' }}
+  >
+    <Modal.Dialog>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal title</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <p>Modal body text goes here.</p>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary">Close</Button>
+        <Button variant="primary">Save changes</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  </div>
   );
 };
 
