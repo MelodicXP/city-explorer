56e35e0e773076458ea57f09d9d78fd33c792f80
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-11-20 06:25:07 -0600

    Refactor

--- src/components/ErrorModal.jsx
+++ src/components/ErrorModal.jsx
@@ -1,33 +1,33 @@
-import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+  import PropTypes from 'prop-types';
+  import Button from 'react-bootstrap/Button';
+  import Modal from 'react-bootstrap/Modal';
 
-const ErrorModal = (props) => {
-  const { errorTitle, errorBody, show, toggleModal } = props;
- 
-  return (
-    <>
-      <Modal show={show} onHide={toggleModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>{errorTitle}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{errorBody}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={toggleModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-};
+  const ErrorModal = (props) => {
+    const { errorTitle, errorBody, show, toggleModal } = props;
+  
+    return (
+      <>
+        <Modal show={show} onHide={toggleModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>{errorTitle}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{errorBody}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={toggleModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  };
 
-// Define prop types
-ErrorModal.propTypes = {
-  errorTitle: PropTypes.string.isRequired,
-  errorBody: PropTypes.string.isRequired,
-  show: PropTypes.bool.isRequired,
-  toggleModal: PropTypes.func.isRequired
-};
+  // Define prop types
+  ErrorModal.propTypes = {
+    errorTitle: PropTypes.string.isRequired,
+    errorBody: PropTypes.string.isRequired,
+    show: PropTypes.bool.isRequired,
+    toggleModal: PropTypes.func.isRequired
+  };
 
-export default ErrorModal;
+  export default ErrorModal;
